1fa5dba8349efc10e6aaa2b82e346f8ffb88e68c
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-01-25 17:40:07 +0100

    Updates

---
+++ precache-manifest.e738251519f07a1607ac4fce3a791846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df12e3c7b16d4009469911fb8c0b230",
+    "url": "/paralax/index.html"
+  },
+  {
+    "revision": "c8c71c78e5386b9c2495",
+    "url": "/paralax/static/css/main.f693de69.chunk.css"
+  },
+  {
+    "revision": "fcf59c0077808a8ca628",
+    "url": "/paralax/static/js/2.34e8b956.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/paralax/static/js/2.34e8b956.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8c71c78e5386b9c2495",
+    "url": "/paralax/static/js/main.8194b169.chunk.js"
+  },
+  {
+    "revision": "88cc0e16d876046e7d58",
+    "url": "/paralax/static/js/runtime-main.b2eb7116.js"
+  }
+]);
