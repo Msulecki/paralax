5d284ca8189064b343f19d44c85064dbdf43e67e
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-01-25 18:45:20 +0100

    Updates

--- precache-manifest.5f36126d15af596b70e5d29d2e7f6c75.js
+++ precache-manifest.5f36126d15af596b70e5d29d2e7f6c75.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "057ab0ea9c7226da2b27e862530d6e1d",
+    "revision": "36ae7eba5be7d143e21cc6351ce70e70",
     "url": "/paralax/index.html"
   },
   {
-    "revision": "4223b015c2799bf57010",
+    "revision": "49a4fb6e13cffcfd240a",
     "url": "/paralax/static/css/main.f693de69.chunk.css"
   },
   {
-    "revision": "fcf59c0077808a8ca628",
-    "url": "/paralax/static/js/2.34e8b956.chunk.js"
+    "revision": "1afa493b6226f05229b5",
+    "url": "/paralax/static/js/2.1b1d2a7a.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/paralax/static/js/2.34e8b956.chunk.js.LICENSE"
+    "url": "/paralax/static/js/2.1b1d2a7a.chunk.js.LICENSE"
   },
   {
-    "revision": "4223b015c2799bf57010",
-    "url": "/paralax/static/js/main.2672e307.chunk.js"
+    "revision": "49a4fb6e13cffcfd240a",
+    "url": "/paralax/static/js/main.2d7ca39f.chunk.js"
   },
   {
     "revision": "88cc0e16d876046e7d58",
